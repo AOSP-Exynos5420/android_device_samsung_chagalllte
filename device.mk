38dc196a701b1387292f5f09c7c0ce921f0b972f
Sean Hoyt <seanhoyt963@gmail.com>
2017-03-04 02:41:02 -0600

    Set proper mqanelements value
    Should fix carrier searching

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/lib/libsec-ril.so
     rild.libargs=-d /dev/ttyS0
     ro.telephony.default_network=9
-    ro.ril.telephony.mqanelements=6
+    ro.ril.telephony.mqanelements=5
 
 # setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
